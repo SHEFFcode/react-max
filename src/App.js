3a584ea5e12439995f1d57d2a9c0b785f04d80e1
SHEFFmachine <sheff@sheffmachine.com>
2018-05-10 21:01:54 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div> Hi I'm a react app</div>
+      </div>
+    );
+  }
+}
+
+export default App;
