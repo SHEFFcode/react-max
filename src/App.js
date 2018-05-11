83f0614d42967373df54eff79b6004d74a79f5b4
SHEFFmachine <sheff@sheffmachine.com>
2018-05-10 22:15:28 -0700

    get going on react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      persons: [
+        { name: 'Jeremy', age: 29 },
+        { name: 'Manu', age: 28 },
+        { name: 'Stephanie', age: 26 },
+      ]
+    }
+  }
+
+  onSwitchName() {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 29 },
+        { name: 'Manu', age: 28 },
+        { name: 'Stephanie', age: 27 },]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <div> Hi I'm a react app</div>
+        <button onClick={this.onSwitchName.bind(this)}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My hobbies are wrestling</Person>
       </div>
     );
   }
